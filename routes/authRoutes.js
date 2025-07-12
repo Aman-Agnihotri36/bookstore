fa43e22536a261ac5f90d705608e01d22fbab45f
Aman-Agnihotri36 <153537979+Aman-Agnihotri36@users.noreply.github.com>
2025-07-12 17:21:27 +0530

    Update authRoutes.js jwt

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,7 +6,7 @@ import jwt from 'jsonwebtoken'
 const router = express.Router();
 
 const generateToken = (userId) => {
-  return jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: "15d" });
+  return jwt.sign({ userId }, process.env.SECRET_KEY, { expiresIn: "15d" });
 };
 
 router.post("/register", async (req, res) => {
