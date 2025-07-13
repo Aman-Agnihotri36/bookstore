82ff92d4287cd615e266544366759ef52cac2112
Aman-Agnihotri36 <153537979+Aman-Agnihotri36@users.noreply.github.com>
2025-07-13 09:17:59 +0530

    Update authRoutes.js photo

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -43,7 +43,7 @@ router.post("/register", async (req, res) => {
       email,
       username,
       password: hashedPassword,
-      profileImage,
+      photo: profileImage,
     });
 
     await user.save();
