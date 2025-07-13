b2fc0a152af2ec7f5fa5629751ce2cbcfe359a7e
Aman-Agnihotri36 <153537979+Aman-Agnihotri36@users.noreply.github.com>
2025-07-13 12:56:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 job.start()
-app.use(express.json());
+// Increase body size limit
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+
 app.use(cors())
 
 
