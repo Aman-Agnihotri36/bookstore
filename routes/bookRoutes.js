34d5f88b5f899f5937a76b66b440f9a06a560602
Aman-Agnihotri36 <153537979+Aman-Agnihotri36@users.noreply.github.com>
2025-07-13 14:10:13 +0530

    Update bookRoutes.js

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -72,34 +72,33 @@ router.get("/user", isAuthenticated, async (req, res) => {
     }
 })
 
-router.delete('/:id', async (req, res) => {
-    try {
-        const book = await Book.findById(req.params.id)
+router.delete("/:id", protectRoute, async (req, res) => {
+  try {
+    const book = await Book.findById(req.params.id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
 
-        if (!book) {
-            return res.status(404).json({ message: "Book not found" })
-        }
+    // check if user is the creator of the book
+    if (book.user.toString() !== req.user._id.toString())
+      return res.status(401).json({ message: "Unauthorized" });
 
-        if (book.user.toString() !== req.user.id.toString()) {
-            return res.status(401).json({ message: "Unathorize" })
-        }
-
-        if (book.image && book.image.includes("cloudinary")) {
-            try {
-                const publicId = book.image.split("/").pop().split(".")[0]
-                await cloudinary.uploader.destroy(publicId)
-            } catch (error) {
-                console.log("Erro deleting image from cloudinary", error)
-            }
-        }
-
-        await book.deleteOne()
-
-        res.json({ message: "Book deleted successfully" })
-    } catch (error) {
-        console.log("Error deleting book", error)
-        return res.status(401).json({ message: "Internal Server Error " })
+    // https://res.cloudinary.com/de1rm4uto/image/upload/v1741568358/qyup61vejflxxw8igvi0.png
+    // delete image from cloduinary as well
+    if (book.image && book.image.includes("cloudinary")) {
+      try {
+        const publicId = book.image.split("/").pop().split(".")[0];
+        await cloudinary.uploader.destroy(publicId);
+      } catch (deleteError) {
+        console.log("Error deleting image from cloudinary", deleteError);
+      }
     }
-})
+
+    await book.deleteOne();
+
+    res.json({ message: "Book deleted successfully" });
+  } catch (error) {
+    console.log("Error deleting book", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
 
 export default router
